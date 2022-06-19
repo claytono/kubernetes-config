9ba36338513b701fadede386c717835ad03c3081
Clayton O'Neill <clayton@oneill.net>
2022-06-19 09:52:22 -0400

    Add VPA for home assistant
    This is running in recommend only mode for now.

---
+++ metrics-server/render-helm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$BASEDIR"
+
+rm -rf helm tmp
+mkdir tmp
+helm template metrics-server metrics-server \
+  --repo https://kubernetes-sigs.github.io/metrics-server/ \
+  --version 3.8.2 \
+  --values values.yaml \
+  --output-dir tmp
+
+mv tmp/*/* helm
+rmdir tmp/*
+rmdir tmp
---
+++ vpa/render-helm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$BASEDIR"
+
+rm -rf helm tmp
+mkdir tmp helm
+helm template vpa vpa \
+  --repo https://charts.fairwinds.com/stable \
+  --include-crds \
+  --version 1.4.0 \
+  --values values.yaml \
+  --output-dir tmp
+
+mv tmp/*/* helm
+rmdir tmp/*
+rmdir tmp
+rm -rf helm/tests
