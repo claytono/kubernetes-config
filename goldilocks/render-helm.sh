990b7bc8acde3135396af5eb9c8f175ed94cdd9a
Clayton O'Neill <clayton@oneill.net>
2022-06-27 19:17:29 -0400

    Add goldilocks

---
+++ goldilocks/render-helm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$BASEDIR"
+
+REPO=https://charts.fairwinds.com/stable
+NAME=goldilocks
+VERSION=6.1.3
+
+if [ ! -f values.yaml ]; then
+  touch values.yaml
+fi
+
+rm -rf helm tmp
+mkdir tmp helm
+helm template "$NAME" "$NAME" \
+  --repo "$REPO" \
+  --include-crds \
+  --version "$VERSION" \
+  --values values.yaml \
+  --output-dir tmp
+
+mv tmp/*/* helm
+rmdir tmp/*
+rmdir tmp
+rm -rf helm/tests
