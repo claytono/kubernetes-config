5f6e0d1a19d2949f4ae609445ba31b9239046d70
Clayton O'Neill <clayton@oneill.net>
2022-09-20 20:09:52 -0400

    First pass at moving to restic for backups

---
+++ resticprofile/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+image=$(yq .spec.jobTemplate.spec.template.spec.containers[0].image <cronjob.yaml)
+
+mkdir -p "$(pwd)/.cache"
+
+docker run -it --rm \
+    --env-file secret.txt \
+    -v "$(pwd)/.cache:/cache" \
+    -v "$(pwd)/profiles.yaml:/resticprofile-config/profiles.yaml:ro" \
+    -v "$(pwd)/rclone-config-secret.txt:/rclone-config/rclone.conf:ro" \
+    "$image" \
+    -c /resticprofile-config/profiles.yaml \
+    --lock-wait 6h \
+    "$@"
