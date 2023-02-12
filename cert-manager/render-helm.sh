dc85298829db830cdc429c18a884d7b5a134068a
Clayton O'Neill <clayton@oneill.net>
2023-02-12 12:17:17 -0500

    Switch cert-manager to helm
    This also sets the defaultissuer so I can stop setting the annotation on
    all ingresses.

---
+++ cert-manager/render-helm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$BASEDIR"
+
+rm -rf helm tmp
+mkdir tmp helm
+helm template cert-manager cert-manager \
+  --repo https://charts.jetstack.io \
+  --create-namespace \
+  --include-crds \
+  --version 1.11.0 \
+  --values values.yaml \
+  --output-dir tmp
+
+mv tmp/*/* helm
+rmdir tmp/*
+rmdir tmp
+rm -rf helm/tests
