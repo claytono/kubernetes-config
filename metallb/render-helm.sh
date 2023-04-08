3a1e02e8298782338ab1b9e8d6f87c010faa106c
Clayton O'Neill <clayton@oneill.net>
2023-04-08 10:48:45 -0400

    Upgrade metallb and convert to helm

---
+++ metallb/render-helm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$BASEDIR"
+
+REPO=https://metallb.github.io/metallb
+NAME=metallb
+VERSION=0.13.9
+
+if [ ! -f values.yaml ]; then
+  touch values.yaml
+fi
+
+rm -rf helm tmp
+mkdir tmp helm
+helm template "$NAME" "$NAME" \
+  --repo "$REPO" \
+  --include-crds \
+  --version "$VERSION" \
+  --values values.yaml \
+  --output-dir tmp
+
+mv tmp/*/* helm
+rmdir tmp/*
+rmdir tmp
+rm -rf helm/tests
