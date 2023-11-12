7d805fb72d867f9976fbd6def9e5c677647150cc
Clayton O'Neill <clayton@oneill.net>
2023-11-12 10:11:33 -0500

    Add node-feature-discovery

---
+++ node-feature-discovery/render-helm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$BASEDIR"
+
+rm -rf helm tmp
+mkdir tmp helm
+helm template node-feature-discovery \
+  --version 0.14.3 \
+  --repo https://kubernetes-sigs.github.io/node-feature-discovery/charts \
+  --values values.yaml \
+  --include-crds \
+  --namespace node-feature-discovery \
+  --create-namespace \
+  --generate-name \
+  --output-dir tmp
+
+mv tmp/*/* helm
+rmdir tmp/*
+rmdir tmp
+rm -rf helm/tests
