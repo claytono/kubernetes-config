0d79dd857503f95f5b1a87be877d1ff080738875
Clayton O'Neill <clayton@oneill.net>
2023-11-12 09:40:21 -0500

    Move restic to azure

--- resticprofile/run.sh
+++ resticprofile/run.sh
@@ -2,7 +2,7 @@
 
 set -eu -o pipefail
 
-image=$(yq .spec.jobTemplate.spec.template.spec.containers[0].image <cronjob-default.yaml)
+image=$(yq .spec.jobTemplate.spec.template.spec.containers[0].image <cronjob-azure.yaml)
 
 mkdir -p "$(pwd)/.cache"
 mkdir -p "$(pwd)/.tmp"
