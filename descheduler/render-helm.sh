b2a8d62d7d3d5bf843458b9e7ac5dfa84a2c8703
Clayton O'Neill <clayton@oneill.net>
2024-02-11 19:25:54 -0500

    Add descheduler

---
+++ descheduler/render-helm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$BASEDIR"
+
+rm -rf helm tmp
+mkdir tmp helm
+helm template descheduler descheduler \
+  --repo https://kubernetes-sigs.github.io/descheduler/ \
+  --version 0.26.0 \
+  --values values.yaml \
+  --output-dir tmp
+
+mv tmp/*/* helm
+rmdir tmp/*
+rmdir tmp
+rm -rf helm/tests
