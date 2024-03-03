c1c351b2296d541e60aebf377236f7be4d97eac9
Clayton O'Neill <clayton@oneill.net>
2024-03-02 23:59:25 -0500

    Add GYB for gmail backup

---
+++ got-your-back/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -xve
+
+VERSION=1.80
+ARCH=$(uname -m)
+URL="https://github.com/GAM-team/got-your-back/releases/download/v1.80/gyb-${VERSION}-linux-${ARCH}-glibc2.31.tar.xz"
+
+cd /tmp
+apt update
+apt install curl xz-utils -y
+
+curl -L -o gyb.tar.xz $URL
+tar xvf gyb.tar.xz
+mv gyb/gyb /usr/local/bin
+rm -rf gyb gyb.tar.xz
+
+cd /data
+EMAIL_ADDRESS=$(echo $EMAIL_ADDRESS | tr -d '\n')
+cp /config-source/account.secret.json "/config/${EMAIL_ADDRESS}.cfg"
+
+flock --verbose --wait 14400 /data/lock \
+  gyb --config /config/  --email "${EMAIL_ADDRESS}" "$@"
+
+if [ -n "$PING_URL" ]; then
+  curl -fsS -m 10 --retry 5 "$PING_URL"
+fi
