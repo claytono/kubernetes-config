2760929aafa0423a6a430993f4cceeced3ffd5bb
Clayton O'Neill <clayton@oneill.net>
2024-03-10 09:08:01 -0400

    Add kube-state-metrics

---
+++ kube-state-metrics/render-helm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$BASEDIR"
+
+rm -rf helm tmp
+mkdir tmp helm
+helm template kube-state-metrics kube-state-metrics \
+  --repo https://prometheus-community.github.io/helm-charts \
+  --include-crds \
+  --version 4.20.2 \
+  --values values.yaml \
+  --output-dir tmp
+
+mv tmp/*/* helm
+rmdir tmp/*
+rmdir tmp
+rm -rf helm/tests
