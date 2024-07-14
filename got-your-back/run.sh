4614848864eb82579662aea0e870f5041c04b274
Clayton O'Neill <clayton@oneill.net>
2024-07-14 15:31:48 -0400

    Update GYB to use external secrets

--- got-your-back/run.sh
+++ got-your-back/run.sh
@@ -8,17 +8,25 @@ cd /tmp
 apt update
 apt install curl xz-utils -y
 
-curl -L -o gyb.tar.xz $URL
+curl -L -o gyb.tar.xz "$URL"
 tar xvf gyb.tar.xz
 mv gyb/gyb /usr/local/bin
 rm -rf gyb gyb.tar.xz
 
 cd /data
-EMAIL_ADDRESS=$(echo $EMAIL_ADDRESS | tr -d '\n')
-cp /config-source/account.secret.json "/config/${EMAIL_ADDRESS}.cfg"
+
+cp -aL /config-source/* /config
+
+old_filename=$(find /config -name "*.cfg")
+new_filename="${old_filename//_/@}"
+mv "$old_filename" "$new_filename"
+
+email_address=$(basename "$new_filename" .cfg | grep -oP '[^/]+$' | sed 's/\.cfg//')
+
+echo "Email address extracted and saved: $email_address"
 
 flock --verbose --wait 14400 /data/lock \
-  gyb --config /config/  --email "${EMAIL_ADDRESS}" "$@"
+  gyb --config /config/  --email "$email_address" "$@"
 
 if [ -n "$PING_URL" ]; then
   curl -fsS -m 10 --retry 5 "$PING_URL"
