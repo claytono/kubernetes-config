1d98ca095eb89b966008a6042adfc9ec2d92d9fa
Clayton O'Neill <clayton@oneill.net>
2025-07-13 14:19:59 -0400

    Remove hashes that change on every render

--- n8n/render-helm.sh
+++ n8n/render-helm.sh
@@ -20,6 +20,10 @@ mv tmp/n8n/templates/* helm/n8n/
 mv tmp/n8n/charts/valkey/* helm/valkey/
 rm -f helm/valkey/templates/secret.yaml
 
+# Remove checksum/secret annotation from valkey application.yaml
+# Use yq to remove the specific checksum/secret annotation
+yq -i 'del(.spec.template.metadata.annotations["checksum/secret"])' helm/valkey/templates/primary/application.yaml
+
 
 # Render PostgreSQL chart
 helm_template n8n-postgres postgresql --namespace n8n --values postgres-values.yaml --output-dir tmp
